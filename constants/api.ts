ac8198c6e79f3fa3743eea3f4df25bf07883d7a2
Abdullah Al Sayed <sayedabdullahal11@gmail.com>
2025-12-30 22:25:56 +0600

    live server added

--- constants/api.ts
+++ constants/api.ts
@@ -1,6 +1,4 @@
-// export const API_BASE_URL = 'https://dining-token-yrij.vercel.app'; 
-
-const IP_ADDRESS = '192.168.0.194.nip.io';
-const PORT = '3000';
-export const API_BASE_URL = `http://${IP_ADDRESS}:${PORT}`;
-export const SOCKET_URL = `http://${IP_ADDRESS}:${PORT}`;
+//const IP_ADDRESS = '192.168.0.194.nip.io';
+//const PORT = '3000';
+export const API_BASE_URL = 'https://dining-token.onrender.com';
+export const SOCKET_URL = 'https://dining-token.onrender.com';
